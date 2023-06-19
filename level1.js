fdc2abe66190f734e800cac2ab4cb6504f203cdf
phoebe-whitley <120589068+phoebe-whitley@users.noreply.github.com>
2023-06-19 21:15:46 +0100

    Add files via upload

--- level1.js
+++ level1.js
@@ -16,7 +16,7 @@ window.onload = function() {
                 gravity: {
                     y: 0
                 },
-                debug: true,
+                debug: false,
             }
         }
     }
@@ -24,39 +24,52 @@ window.onload = function() {
     window.focus();
 
 }
+//document.body.style = 'margin:0;';
+let counter = 0;
+let counterText; // variable for text object
+let updateText;
+this.counterText = null;
 class scene1 extends Phaser.Scene{
     constructor(){
         super("PlayGame");
     }
+create(){
 
     
-    create(){
-
-        this.matter.world.update30Hz();
-        this.matter.world.setBounds(10, 10, level1.config.width - 20, level1.config.height - 10);
-        const rect = this.add.rectangle(level1.config.width / 2, level1.config.width / 3, 600, 100, 0xff0000);
+    let path = [{x:0, y:0}, {x:level1.config.width, y:0}, {x:level1.config.width, y:50},{x: 0, y:50} ];
+        this.polygon = this.matter.add.fromVertices(level1.config.width/2, 250, path, { isStatic: true });
+        this.polygon.gameObject = this.add.polygon(level1.config.width/2, 250, path, 0xff0000);
         this.lineGraphics = this.add.graphics();
         this.input.on("pointerdown", this.startDrawing, this);
         this.input.on("pointerup", this.stopDrawing, this);
         this.input.on("pointermove", this.keepDrawing, this);
         this.isDrawing = false;
         this.add.text(13, 11, 'Level 1',{fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'});
-
-
+        counterText = this.add.text(13, 32, 'Attempts: ' + counter, {fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'});
     }
-
-    startDrawing(){
+    
+    startDrawing(pointer){
         this.isDrawing = true;
+        counter = counter + 1;
     }
+
+    update(){
+        if (counter > 0){
+            counterText.setVisible(false);
+            counterText = this.add.text(13, 32,'Attempts: ' + counter, {fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'});
+        }
+         
+    }
+
     keepDrawing(pointer){
         if(this.isDrawing){
             this.lineGraphics.clear();
-            this.lineGraphics.lineStyle(1, 0xff0000);
+            this.lineGraphics.lineStyle(5, 'black');
             this.lineGraphics.moveTo(pointer.downX, pointer.downY);
             this.lineGraphics.lineTo(pointer.x, pointer.y);
             this.lineGraphics.strokePath();
-        }
-    }
+    }}
+    
     stopDrawing(pointer){
         this.lineGraphics.clear();
         this.isDrawing = false;
@@ -70,37 +83,40 @@ class scene1 extends Phaser.Scene{
                 pointsArray.push(vertex.x, vertex.y)
             });
             let slicedPolygons = PolyK.Slice(pointsArray, pointer.downX, pointer.downY, pointer.upX, pointer.upY);
+
+
             if(slicedPolygons.length > 1){
+                this.add.text(13, 11, 'Level 1 complete!',{fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'});
+
                 toBeSliced.push(bodies[i]);
                 slicedPolygons.forEach(function(points){
                     toBeCreated.push(points)
-
-                })
-
+                });
             }
-        }
-        toBeSliced.forEach(function(body){
-            this.matter.world.remove(body)
+        }   
+        toBeSliced.forEach(function(body){ 
+              body.gameObject.destroy();
+              this.matter.world.remove(body)
         }.bind(this))
+        
         toBeCreated.forEach(function(points){
             let polyObject = [];
             for(let i = 0; i < points.length / 2; i ++){
                 polyObject.push({
-                    x: points[i * 2],
-                    y: points[i * 2 + 1]
+                    x: points[i * 2] ,
+                    y: points[i * 2 + 1] 
                 })
             }
             let sliceCentre = Phaser.Physics.Matter.Matter.Vertices.centre(polyObject)
-            let slicedBody = this.matter.add.fromVertices(sliceCentre.x, sliceCentre.y, polyObject, {
-                isStatic: false
-            });
-            this.add.text(13, 11, 'Level 1 Complete!',{fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'});
+            let slicedBody = this.matter.add.fromVertices(sliceCentre.x, sliceCentre.y, polyObject, { isStatic: true });
+            let topleft = this.matter.bodyBounds.getTopLeft(slicedBody); 
+            slicedBody.gameObject = this.add.polygon(topleft.x, topleft.y, polyObject, 0xff0000);
+
+
+
+            }.bind(this));
+
+        }
+    };
     
-        }.bind(this))
-    }
-
-    
-
-};
-
 
