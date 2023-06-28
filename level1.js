4d5281504cf6e2db017f34d4e8d961de16105c54
phoebe-whitley <120589068+phoebe-whitley@users.noreply.github.com>
2023-06-28 11:29:26 +0100

    Add files via upload

--- level1.js
+++ level1.js
@@ -2,12 +2,13 @@
 let level1;
 window.onload = function() {
     let gameConfig = {
+        transparent: true,
         type: Phaser.AUTO,
         scale: {
             mode: Phaser.Scale.FIT,
             autoCenter: Phaser.Scale.CENTER_BOTH,
             parent: "thegame",
-            width: 600,
+            width: 800,
             height: 700
         },
         scene: scene1,
@@ -25,47 +26,48 @@ window.onload = function() {
     window.focus();
 
 }
-//document.body.style = 'margin:0;';
 let counter1 = 0;
+let levelText;
 let counterText; // variable for text object
 this.counterText = null;
 class scene1 extends Phaser.Scene{
     constructor(){
         super("PlayGame");
+        this.light = null;
     }
 
 
 
 create(){
 
-    //mask for graphic effects
-
-    var graphics = this.add.graphics();
-    var mask = new Phaser.Display.Masks.GeometryMask(this, graphics);
-    graphics.fillStyle(0xff0000, 0.5);
-    graphics.fillRect(0, 0, 800, 700);
-
-    var image = this.add.image(400, 300, 'blood.jpg');
-    image.mask = mask;
-    //maskSprite.setTexture('image');
-  
-    //this.mask.gameObject.setMask(mask);
-    //mask.bringToTop();
-
-    //var shape = this.add.graphics();
-    //var mask = shape.add.bitmapMask();
-
     let path = [{x:0, y:0}, {x:level1.config.width, y:0}, {x:level1.config.width, y:50},{x: 0, y:50} ];
-        this.polygon = this.matter.add.fromVertices(level1.config.width/2, 250, path, { isStatic: true });
-        this.polygon.gameObject = this.add.polygon(level1.config.width/2, 250, path, '0xff0000');
-        this.lineGraphics = this.add.graphics();
-        this.input.on("pointerdown", this.startDrawing, this);
-        this.input.on("pointerup", this.stopDrawing, this);
-        this.input.on("pointermove", this.keepDrawing, this);
-        this.isDrawing = false;
-        this.add.text(13, 11, 'Level 1',{fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'});
-        counterText = this.add.text(13, 32, 'Attempts: ' + counter1, {fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'});
-    }
+    this.polygon = this.matter.add.fromVertices(level1.config.width/2, 250, path, { isStatic: true });
+    this.polygon.gameObject = this.add.polygon(level1.config.width/2, 250, path, '0xff0000');
+    this.lineGraphics = this.add.graphics();
+    this.input.on("pointerdown", this.startDrawing, this);
+    this.input.on("pointerup", this.stopDrawing, this);
+    this.input.on("pointermove", this.keepDrawing, this);
+    this.isDrawing = false;
+    levelText = this.add.text(13, 11, 'Level 1',{fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'});
+    counterText = this.add.text(13, 32, 'Attempts: ' + counter1, {fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'});
+
+    //torch code
+    this.light = this.add.circle(0, 0, 30, 0x000000, 1);
+    this.light.visible = false;
+
+    this.input.on(Phaser.Input.Events.POINTER_MOVE, this.handlePointermove, this);
+}
+
+handlePointerMove(pointer){
+
+    const x = pointer.x - this.cover.x + this.cover.width * 0.5
+    const y = pointer.y - this.cover.y + this.cover.height * 0.5
+
+    this.renderTexture.clear()
+    this.renderTexture.draw(this.light, x, y)
+}
+
+    
 
 
  
@@ -108,7 +110,8 @@ create(){
 
 
             if(slicedPolygons.length > 1){
-                this.add.text(13, 11, 'Level 1 complete!',{fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'});
+                levelText.setVisible(false);
+                this.add.text(160, 210, 'Level 1 complete!',{fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', fontSize: '70px'}).bringToTop();
 
                 toBeSliced.push(bodies[i]);
                 slicedPolygons.forEach(function(points){
@@ -134,9 +137,8 @@ create(){
             let topleft = this.matter.bodyBounds.getTopLeft(slicedBody); 
             slicedBody.gameObject = this.add.polygon(topleft.x, topleft.y, polyObject, 0xff0000);
 
-
-
             }.bind(this));
+
 
         }
     };
